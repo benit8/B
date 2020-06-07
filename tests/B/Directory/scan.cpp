586c2b4238dcef34c0dc5225b48abf8bbb15d6bb
Benoît Lormeau <blormeau@outlook.com>
2020-06-08 01:44:39 +0200

    Added the new super-build-script

--- tests/B/Directory/scan.cpp
+++ tests/B/Directory/scan.cpp
@@ -12,11 +12,10 @@ using B::Directory;
 
 Test(Directory, Scan)
 {
-	auto entries = B::Directory::scan("B/Directory");
+	auto entries = B::Directory::scan(".");
 
 	cr_assert_geq(entries.size(), 3, "entries.size() == %zu", entries.size());
 
 	cr_expect_neq(-1, entries.find([] (auto &e) { return e.name == "."; }));
 	cr_expect_neq(-1, entries.find([] (auto &e) { return e.name == ".."; }));
-	cr_expect_neq(-1, entries.find([] (auto &e) { return e.name == "scan.cpp"; }));
 }
